aa16d04ed227cce30afc704dcd2854966ad5ef09
Zach Robinson <zrobinson@pivotal.io>
2016-07-01 14:45:00 -0700

    WIP - initial commit with migration
    Signed-off-by: Tim Hausler <thausler@pivotal.io>

--- spec/api/api_version_spec.rb
+++ spec/api/api_version_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'vcap/digester'
 
 RSpec.describe 'Stable API warning system', api_version_check: true do
-  API_FOLDER_CHECKSUM = '77ac37d30231a7f610b83357127a5e5b8517bd1a'.freeze
+  API_FOLDER_CHECKSUM = '42e6f86aa99d00b5f7b5ea445c86f8fdbdcd5b75'.freeze
 
   it 'double-checks the version' do
     expect(VCAP::CloudController::Constants::API_VERSION).to eq('2.59.0')
